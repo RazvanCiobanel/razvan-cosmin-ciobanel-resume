ad41d7d093ef601b543844f75e1997d90b206903
RazvanCiobanel <razvanciobanel@tuta.io>
2022-03-11 17:09:12 +0200

    Razvan Ciobanel - updates

--- src/Components/PersInfo.jsx
+++ src/Components/PersInfo.jsx
@@ -1,84 +1,100 @@
-import React from 'react'
-import photo from '../photo.JPG'
-import styled from 'styled-components'
+import React from "react";
+import photo from "../photo.jpg";
+import styled from "styled-components";
 
 const Photo = styled.img`
-    height: auto;
-    width: 100%;
-    max-width: 200px;
-    border-radius: 50%;
-    margin-left: auto;
-    margin-right: auto;
-`
+  height: auto;
+  width: 100%;
+  max-width: 200px;
+  border-radius: 50%;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const Email = styled.p`
-    overflow-wrap: break-word;
-`
+  overflow-wrap: break-word;
+`;
 
 const MyLink = styled.a`
+  text-decoration: none;
+  color: #155171;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:active,
+  &:mailto {
     text-decoration: none;
-    color: #155171;
-    
-
-    &:focus, &:hover, &:visited, &:active, &:Mailto {
-        text-decoration: none;
-    };
-    @media all and (min-width: 401px) {
-        color: white;
-    }
-`
-
+  }
+  @media all and (min-width: 401px) {
+    color: white;
+  }
+`;
 
 const PersInfo = () => {
-    const Mailto = ({ email, subject = '', body = '', children }) => {
-        let params = subject || body ? '?' : '';
-        if (subject) params += `subject=${encodeURIComponent(subject)}`;
-        if (body) params += `${subject ? '&' : ''}body=${encodeURIComponent(body)}`;
+  const Mailto = ({ email, subject = "", body = "", children }) => {
+    let params = subject || body ? "?" : "";
+    if (subject) params += `subject=${encodeURIComponent(subject)}`;
+    if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
 
-        return <MyLink href={`mailto:${email}${params}`}>{children}</MyLink>;
-    };
-    return (
-        <div className="personal-info">
-            <Photo src={photo} alt="Razvan-Cosmin Ciobanel" />
-            <h2>Razvan-Cosmin Ciobanel</h2>
-            <br />
-            <div className="container">
-                <h2>Contact</h2>
-                <p className="text">Email:</p>
-                <Email><Mailto>razvanciobanel@tuta.io</Mailto></Email> <br />
-                <p className="text">Mobile phone:</p>
-                <p>
-                    <MyLink href="tel:0040767206722"
-                        rel="noopener noreferrer"> 0040767206722
-                    </MyLink>
-                </p> <br />
-                <p className="text">
-                    <MyLink href="https://www.linkedin.com/in/razvan-cosmin-ciobanel-18237221b"
-                        target="_blank"
-                        rel="noopener noreferrer">LinkedIn Profile
-                    </MyLink>
-                </p> <br />
-                <p className="text">
-                    <MyLink href="https://github.com/RazvanCiobanel"
-                        target="_blank"
-                        rel="noopener noreferrer">GitHub Profile
-                    </MyLink>
-                </p>
-            </div><br />
-            <div className="container">
-                <h2>Self definition</h2>
-                <p>
-                I am willing to learn new front end and back end technologies. 
-                I have a good understanding of responsive web design and SEO practices. 
-                I am a consistent person and I like to achieve my goals.
-                </p>
-            </div> <br />
-            <div className="container">
-                <h2>Spoken languages</h2>
-                <p className="text">English - C2 Advanced level</p>
-            </div>
-        </div>
-    )
-}
+    return <MyLink href={`mailto:${email}${params}`}>{children}</MyLink>;
+  };
+  return (
+    <div className="personal-info">
+      <Photo src={photo} alt="Razvan-Cosmin Ciobanel" />
+      <h2>Razvan-Cosmin Ciobanel</h2>
+      <br />
+      <div className="container">
+        <h2>Contact</h2>
+        <p className="text">Email:</p>
+        <Email>
+          <Mailto>razvanciobanel@tuta.io</Mailto>
+        </Email>{" "}
+        <br />
+        <p className="text">Mobile phone:</p>
+        <p>
+          <MyLink href="tel:0040767206722" rel="noopener noreferrer">
+            {" "}
+            0040767206722
+          </MyLink>
+        </p>{" "}
+        <br />
+        <p className="text">
+          <MyLink
+            href="https://www.linkedin.com/in/razvan-cosmin-ciobanel-18237221b"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn Profile
+          </MyLink>
+        </p>{" "}
+        <br />
+        <p className="text">
+          <MyLink
+            href="https://github.com/RazvanCiobanel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Profile
+          </MyLink>
+        </p>
+      </div>
+      <br />
+      <div className="container">
+        <h2>Self definition</h2>
+        <p>
+          I am willing to learn new front end and back end technologies. I have
+          a good understanding of responsive web design and SEO practices. I am
+          a consistent person and I like to achieve my goals.
+        </p>
+      </div>{" "}
+      <br />
+      <div className="container">
+        <h2>Spoken languages</h2>
+        <p className="text">English - C2 Advanced level</p>
+      </div>
+    </div>
+  );
+};
 
-export default PersInfo
+export default PersInfo;
--- src/Components/ResumeObj.jsx
+++ src/Components/ResumeObj.jsx
@@ -1,100 +1,168 @@
-import React from 'react'
-import styled from 'styled-components'
-
+import React from "react";
+import styled from "styled-components";
 
 const MyLink = styled.a`
-    text-decoration: none;
-    color: #155171;;
+  text-decoration: none;
+  color: #155171;
 
-    &:focus, &:hover, &:visited, &:active {
-        text-decoration: none;
-    }
-`
+  &:focus,
+  &:hover,
+  &:visited,
+  &:active {
+    text-decoration: none;
+  }
+`;
 
 const ResumeObj = () => {
-    return (
-        <div className="resume-obj">
-            <div className="container">
-                <p className="resume-p">
-                    Highly motivated Merchant Navy Third Officer, I completed <cite>Front End Developer Course</cite> at Codecool Romania. 
-                    I am eager to improve my front end developer skills in designing, building and testing user-oriented and responsive web applications.
-                </p>
-                <br/>
-                <hr/>
-                <br/>
-            </div>
-            <div className="work-edu-container">
-                <h2 className="header">Work experience</h2>
-                <p className="period">May 2015 - January 2021</p>
-                <p className="job-edu">Merchant Navy Third Officer - In charge with safety of navigation.</p>
-                <ul className="skills">
-                    <p>Skills gained:</p>
-                    <li>Good team player with great communication skills - as a third officer I worked in multinational teams;</li>
-                    <li>I am adaptable and I integrate well into any team - every time I was going at sea I had other co-workers;</li>
-                    <li>Experience in organizing a team - during my shifts I was team leader.</li>
-                </ul>
-            </div>
-            <br/>
-            <hr/>
-            <br/>
-            <div className="work-edu-container">
-                <h2 className="header">Education</h2><br/>
-                <p className="period">March 2021 - August 2021</p>
-                <p className="job-edu">Front-end Developer Course at CodeCool Romania Bucharest</p>
-                <ul className="skills">
-                    <li>During this course I learned and worked with HML5, CSS3, JavaScript, Bootstrap and DOM Manipulation;</li>
-                    <li>I designed web applications with JavaScript Framework, React JS. For styling I used CSS3 together with React-Bootstrat and Styled Components packages;</li>
-                    <li>I learned and applied the principles of responsive web design;</li>
-                    <li>I worked with automatic testing environments for testing React components, React Testing Library;</li>
-                    <li>I learned and used HTTP methods for calling a REST API: GET, PUT, POST, PATCH, DELETE;</li>
-                    <li>Also during this course I enhanced some of my soft skills: creativity, problem solving, growth mindset.</li>
-                </ul><br/>
-                <p className="period">October 2014 - January 2016</p>
-                <p className="job-edu">Master Degree in Navigation and Naval Engineering at Constanta Maritime University</p><br/>
-                <p className="period">October 2009 - July 2013</p>
-                <p className="job-edu">Bachelor Degree in Navigation and Maritime and Inland Waterway Transport at Constanta Maritime University</p>
-            </div>
-            <br/>
-            <hr/>
-            <br/>
-            <div className="projects">
-                <h2>Projects</h2><br/>  
-                <div>
-                    <h2>WEATHER APPLICATION - <cite>My Weather</cite></h2>
-                    <MyLink href="https://github.com/Sirius1402/weather-application"
-                    target="_blank"
-                    rel="noopener noreferrer">https://github.com/Sirius1402/weather-application</MyLink>
-                    <p>
-                    Weather website made with ReactJS and powered by WeatherAPI.com. 
-                    It provides data for local weather, daily and hourly forecasts and world-wide weather. 
-                    For testing the application I used React Testing Library and Mock Service Worker, to mock API calls.
-                    </p>
-                </div> <br/>             
-                <div>
-                    <h2>CODECOOL TO DO LIST</h2>
-                    <MyLink href="https://github.com/Sirius1402/Serverless-todo-app-http-methods"
-                    target="_blank"
-                    rel="noopener noreferrer">https://github.com/Sirius1402/Serverless-todo-app-http-methods</MyLink>
-                    <p>
-                    A to-do list done with ReactJS. 
-                    In this project I have used the following HTTP request methods to a local server: GET, POST, PATCH, DELETE.
-                    </p>
-                </div><br/>
-                <div>
-                    <h2>CODECOOL BOOK A TABLE</h2>
-                    <MyLink href="https://github.com/Sirius1402/Book-a-table"
-                    target="_blank"
-                    rel="noopener noreferrer">https://github.com/Sirius1402/Book-a-table</MyLink>
-                    <p>
-                    Landing page for a Pizza Restaurant with a form for booking a table. 
-                    In the booking form there are validations for email format and for date and time. 
-                    The project is done with HTML5, CSS3, and JavaScript. The project is responsive.
-                    </p>
-                </div><br/>
-                
-            </div>
+  return (
+    <div className="resume-obj">
+      <div className="container">
+        <p className="resume-p">
+          Highly motivated Merchant Navy Third Officer, I completed{" "}
+          <cite>Front End Developer Course</cite> at Codecool Romania. I am
+          eager to improve my front end developer skills in designing, building
+          and testing user-oriented and responsive web applications.
+        </p>
+        <br />
+        <hr />
+        <br />
+      </div>
+      <div className="work-edu-container">
+        <h2 className="header">Work experience</h2>
+        <p className="period">May 2015 - January 2021</p>
+        <p className="job-edu">
+          Merchant Navy Third Officer - In charge with safety of navigation.
+        </p>
+        <ul className="skills">
+          <p>Skills gained:</p>
+          <li>
+            Good team player with great communication skills - as a third
+            officer I worked in multinational teams;
+          </li>
+          <li>
+            I am adaptable and I integrate well into any team - every time I was
+            going at sea I had other co-workers;
+          </li>
+          <li>
+            Experience in organizing a team - during my shifts I was team
+            leader.
+          </li>
+        </ul>
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div className="work-edu-container">
+        <h2 className="header">Education</h2>
+        <br />
+        <p className="period">March 2021 - August 2021</p>
+        <p className="job-edu">
+          Front-end Developer Course at CodeCool Romania Bucharest
+        </p>
+        <ul className="skills">
+          <li>
+            During this course I learned and worked with HML5, CSS3, JavaScript,
+            Bootstrap and DOM Manipulation;
+          </li>
+          <li>
+            I designed web applications with JavaScript Framework, React JS. For
+            styling I used CSS3 together with React-Bootstrat and Styled
+            Components packages;
+          </li>
+          <li>
+            I learned and applied the principles of responsive web design;
+          </li>
+          <li>
+            I worked with automatic testing environments for testing React
+            components, React Testing Library;
+          </li>
+          <li>
+            I learned and used HTTP methods for calling a REST API: GET, PUT,
+            POST, PATCH, DELETE;
+          </li>
+          <li>
+            Also during this course I enhanced some of my soft skills:
+            creativity, problem solving, growth mindset.
+          </li>
+        </ul>
+        <br />
+        <p className="period">October 2014 - January 2016</p>
+        <p className="job-edu">
+          Master Degree in Navigation and Naval Engineering at Constanta
+          Maritime University
+        </p>
+        <br />
+        <p className="period">October 2009 - July 2013</p>
+        <p className="job-edu">
+          Bachelor Degree in Navigation and Maritime and Inland Waterway
+          Transport at Constanta Maritime University
+        </p>
+      </div>
+      <br />
+      <hr />
+      <br />
+      <div className="projects">
+        <h2>Projects</h2>
+        <br />
+        <div>
+          <h2>
+            WEATHER APPLICATION - <cite>My Weather</cite>
+          </h2>
+          <MyLink
+            href="https://github.com/RazvanCiobanel/weather-application"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/RazvanCiobanel/weather-application
+          </MyLink>
+          <p>
+            Weather website made with ReactJS and powered by WeatherAPI.com. It
+            provides data for local weather, daily and hourly forecasts and
+            world-wide weather. For testing the application I used React Testing
+            Library and Mock Service Worker, to mock API calls.
+          </p>
+        </div>{" "}
+        <br />
+        <div>
+          <h2>E-COMMERCE PLATFORM</h2>
+          <MyLink
+            href="https://github.com/RazvanCiobanel/e-commerce-platform"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/RazvanCiobanel/e-commerce-platform
+          </MyLink>
+          <p>
+            This repository simulates an e-commerce platform. It uses a GraphQL
+            backend. For your information the backend it is not done by me. The
+            frontend it is done by me, in ReactJs, with class components. To
+            manage the global state of application I used redux.Other libraries
+            used for this repository:@apollo/client; dompurify; graphql;
+            graphql-config; react-redux; react-router-dom; redux-persist;
+            redux-thunk; styled-components; weak-key; reselect;
+          </p>
         </div>
-    )
-}
+        <br />
+        <div>
+          <h2>CODECOOL BOOK A TABLE</h2>
+          <MyLink
+            href="https://github.com/RazvanCiobanel/Book-a-table"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/RazvanCiobanel/Book-a-table
+          </MyLink>
+          <p>
+            Landing page for a Pizza Restaurant with a form for booking a table.
+            In the booking form there are validations for email format and for
+            date and time. The project is done with HTML5, CSS3, and JavaScript.
+            The project is responsive.
+          </p>
+        </div>
+        <br />
+      </div>
+    </div>
+  );
+};
 
-export default ResumeObj
+export default ResumeObj;
