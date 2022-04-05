99865c28bcdb83489775fc4d968a96ec3b1a0861
RazvanCiobanel <razvanciobanel@tuta.io>
2022-04-05 14:09:35 +0300

    email

--- src/Components/PersInfo.jsx
+++ src/Components/PersInfo.jsx
@@ -48,7 +48,7 @@ const PersInfo = () => {
         <h2>Contact</h2>
         <p className="text">Email:</p>
         <Email>
-          <Mailto>razvanciobanel@tuta.io</Mailto>
+          <Mailto>razvancciobanel@gmail.com</Mailto>
         </Email>{" "}
         <br />
         <p className="text">Mobile phone:</p>
